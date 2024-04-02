52ecaf2c47bad694da821b662f49e2198cee3eb7
Anish <kirodiwlanish11@gmail.com>
2024-04-02 22:19:37 +0530

    final commit

---
+++ src/app/store.js
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { cryptoApi } from '../services/cryptoApi';
+import { cryptoNewsApi } from '../services/cryptoNewsApi';
+
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(cryptoApi.middleware, cryptoNewsApi.middleware),
+    // Add cryptoNewsApi.middleware
+    // getDefaultMiddleware().concat(cryptoApi.middleware)
+});
+
+
+// export default configureStore({
+//     reducer:{
+//         [cryptoApi.reducerPath]:cryptoApi.reducer,
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware().concat(cryptoApi.middleware),
+
+// })
